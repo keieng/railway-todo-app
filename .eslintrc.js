969dcc889e317ccd15650d7ce65010de9fb1dadb
keieng <105902831+keieng@users.noreply.github.com>
2022-11-13 20:44:49 +0900

    prettier設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,17 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "standard"],
+  extends: ['plugin:react/recommended', 'standard'],
   overrides: [],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["react"],
-  rules: {},
+  plugins: ['react', 'prettier'],
+  rules: { 'comma-dangle': ['error', 'always'] },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
-};
+}
